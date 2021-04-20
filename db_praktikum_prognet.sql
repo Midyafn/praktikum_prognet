5c1f92de08efbc1123e33c62e3c6b3cb2da46874
Midyafn <80409196+Midyafn@users.noreply.github.com>
2021-04-20 14:28:13 +0800

    Add files via upload

---
+++ db_praktikum_prognet.sql
@@ -0,0 +1,376 @@
+/*
+SQLyog Ultimate v12.09 (64 bit)
+MySQL - 10.4.8-MariaDB : Database - db_praktikum_prognet
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_praktikum_prognet` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+
+USE `db_praktikum_prognet`;
+
+/*Table structure for table `admin_notifications` */
+
+DROP TABLE IF EXISTS `admin_notifications`;
+
+CREATE TABLE `admin_notifications` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notifiable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notifiable_id` int(10) unsigned NOT NULL,
+  `data` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `read_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `seller_notifications_notifiable_type_notifiable_id_index` (`notifiable_type`,`notifiable_id`),
+  KEY `notifiable_id` (`notifiable_id`),
+  CONSTRAINT `admin_notifications_ibfk_1` FOREIGN KEY (`notifiable_id`) REFERENCES `admins` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `admin_notifications` */
+
+/*Table structure for table `admins` */
+
+DROP TABLE IF EXISTS `admins`;
+
+CREATE TABLE `admins` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profile_image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `phone` varchar(12) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `sellers_email_unique` (`phone`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `admins` */
+
+insert  into `admins`(`id`,`username`,`password`,`name`,`profile_image`,`phone`,`remember_token`,`created_at`,`updated_at`) values (1,'admin','$2y$10$ZkLbZBQk0HhDHxvAw2Dlzeegvmfyk24wQWV.RtlqHfx27U31ZGSuy','Admin','A','087751101193','5SloOrKkDbULHEl3ME64GIDaBTWB1tR2Ot6bRE9P8E4B9Cyf2sPNCAuB3t2F','2021-03-17 14:17:56','2021-03-17 14:17:56'),(2,'admin2','$2y$10$69NlwtiC07PQIJvpJWspR.UPiWIpMRwRPLpRbVeJxszBCqyU/652O','Admin2','q','089997775334','KReNZnCA2POnbrW10xQfMW6lbYgy7VYDKSAopuIl2rIsw16afaAjnHofbh7X','2021-03-19 14:32:53','2021-03-19 14:32:53');
+
+/*Table structure for table `carts` */
+
+DROP TABLE IF EXISTS `carts`;
+
+CREATE TABLE `carts` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `qty` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `status` enum('checkedout','notyet','cancelled') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `carts_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `carts_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `carts` */
+
+/*Table structure for table `couriers` */
+
+DROP TABLE IF EXISTS `couriers`;
+
+CREATE TABLE `couriers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `courier` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `couriers` */
+
+/*Table structure for table `discounts` */
+
+DROP TABLE IF EXISTS `discounts`;
+
+CREATE TABLE `discounts` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `id_product` int(10) unsigned DEFAULT NULL,
+  `percentage` int(3) DEFAULT NULL,
+  `start` date DEFAULT NULL,
+  `end` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_product` (`id_product`),
+  CONSTRAINT `discounts_ibfk_1` FOREIGN KEY (`id_product`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `discounts` */
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `migration` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`id`,`migration`,`batch`) values (1,'2014_10_12_000000_create_users_table',1),(2,'2014_10_12_100000_create_password_resets_table',1),(3,'2019_02_15_123603_create_admins_table',1),(4,'2019_02_15_123744_create_sellers_table',1),(5,'2019_02_15_125445_create_products_table',1),(6,'2019_02_15_130341_create_product_images_table',1),(7,'2019_02_15_131114_create_transactions_table',1),(8,'2019_02_15_131132_create_transaction_details_table',1),(9,'2019_02_15_132352_create_product_categories_table',1),(10,'2019_02_15_132701_create_carts_table',1),(11,'2019_02_15_134220_create_wishlists_table',1),(12,'2019_02_16_044815_create_rates_table',1),(13,'2019_02_16_045411_create_product_reviews_table',1),(14,'2019_02_16_062504_create_qna_products_table',1),(15,'2019_02_16_063238_create_shopping_voucers_table',1),(16,'2019_02_16_064643_create_couriers_table',1),(17,'2019_02_16_102028_create_notifications_table',1),(18,'2019_02_16_103007_create_seller_notifications_table',1),(19,'2019_02_16_103024_create_user_notifications_table',1);
+
+/*Table structure for table `password_resets` */
+
+DROP TABLE IF EXISTS `password_resets`;
+
+CREATE TABLE `password_resets` (
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `token` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  KEY `password_resets_email_index` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `password_resets` */
+
+/*Table structure for table `product_categories` */
+
+DROP TABLE IF EXISTS `product_categories`;
+
+CREATE TABLE `product_categories` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `product_categories` */
+
+/*Table structure for table `product_category_details` */
+
+DROP TABLE IF EXISTS `product_category_details`;
+
+CREATE TABLE `product_category_details` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) unsigned NOT NULL,
+  `category_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  KEY `category_id` (`category_id`),
+  CONSTRAINT `product_category_details_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`),
+  CONSTRAINT `product_category_details_ibfk_2` FOREIGN KEY (`category_id`) REFERENCES `product_categories` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `product_category_details` */
+
+/*Table structure for table `product_images` */
+
+DROP TABLE IF EXISTS `product_images`;
+
+CREATE TABLE `product_images` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) unsigned NOT NULL,
+  `image_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_images_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `product_images` */
+
+/*Table structure for table `product_reviews` */
+
+DROP TABLE IF EXISTS `product_reviews`;
+
+CREATE TABLE `product_reviews` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `rate` int(1) NOT NULL,
+  `content` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  KEY `rate_id` (`rate`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `product_reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `product_reviews_ibfk_3` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `product_reviews` */
+
+/*Table structure for table `products` */
+
+DROP TABLE IF EXISTS `products`;
+
+CREATE TABLE `products` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `price` int(11) NOT NULL,
+  `description` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `product_rate` double DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `stock` int(10) DEFAULT NULL,
+  `weight` int(3) DEFAULT NULL,
+  `gender` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `images` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `products` */
+
+insert  into `products`(`id`,`product_name`,`price`,`description`,`product_rate`,`created_at`,`updated_at`,`stock`,`weight`,`gender`,`images`) values (1,'DEJSA lampu cream',400000,'- Kaos detail statementable typograph untuk daily look yang nyaman\r\n- Warna salt crystal (beige)\r\n- Kerah bulat\r\n- Unlined\r\n- Regular fit\r\n- Material katun tidak transparan, ringan, dan tidak stretch',NULL,'2021-03-23 01:19:44','2021-03-23 01:19:54',15,0,'wanita','lampu_krem.png'),(2,'TETRIAL lampu putih',425000,'- Brand graphic basic tee with faded effect\r\n- Round neckline\r\n- Unlined\r\n- Regular fit\r\n- Slip on style\r\n- Cotton',NULL,'2021-03-23 01:23:51','2021-03-23 01:23:56',10,NULL,'wanita','lampu_putih.png'),(3,'FINNSTAR lampu biru',300000,'- Daily t-shirt monokrom dengan detail classic art print\r\n- Warna putih\r\n- Kerah bulat\r\n- Unlined\r\n- Regular fit\r\n- Material katun tidak transparan, ringan dan tidak stretch',NULL,'2021-03-23 01:29:13','2021-03-23 01:29:16',15,NULL,'wanita','lampu_biru.png'),(4,'EMMA rak buku ',0,'',NULL,NULL,NULL,NULL,NULL,NULL,'rak_coklat.png'),(5,'BILLY rak buku',0,'',NULL,NULL,NULL,NULL,NULL,NULL,'rak_hitam.png'),(6,'FEJKA tanaman pot',0,'',NULL,NULL,NULL,NULL,NULL,NULL,'tanamanhias.png'),(7,'',0,'',NULL,NULL,NULL,NULL,NULL,NULL,'tanamandahlia.png');
+
+/*Table structure for table `response` */
+
+DROP TABLE IF EXISTS `response`;
+
+CREATE TABLE `response` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `review_id` int(10) unsigned NOT NULL,
+  `admin_id` int(10) unsigned NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `review_id` (`review_id`),
+  KEY `admin_id` (`admin_id`),
+  CONSTRAINT `response_ibfk_1` FOREIGN KEY (`review_id`) REFERENCES `product_reviews` (`id`),
+  CONSTRAINT `response_ibfk_2` FOREIGN KEY (`admin_id`) REFERENCES `admins` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `response` */
+
+/*Table structure for table `responses` */
+
+DROP TABLE IF EXISTS `responses`;
+
+CREATE TABLE `responses` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `review_id` int(10) unsigned NOT NULL,
+  `admin_id` int(10) unsigned NOT NULL,
+  `content` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `review_id` (`review_id`),
+  KEY `admin_id` (`admin_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+
+/*Data for the table `responses` */
+
+insert  into `responses`(`id`,`review_id`,`admin_id`,`content`,`created_at`,`updated_at`) values (4,14,2,'Nanti saya kasi gan','2020-05-30 04:27:53','2020-05-30 04:27:53'),(5,15,2,'Jelek barangnya','2020-05-30 04:53:21','2020-05-30 04:53:21');
+
+/*Table structure for table `transaction_details` */
+
+DROP TABLE IF EXISTS `transaction_details`;
+
+CREATE TABLE `transaction_details` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `transaction_id` int(10) unsigned NOT NULL,
+  `product_id` int(10) unsigned NOT NULL,
+  `qty` int(11) NOT NULL,
+  `discount` int(3) DEFAULT NULL,
+  `selling_price` int(11) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_transaction` (`transaction_id`),
+  KEY `id_product` (`product_id`),
+  CONSTRAINT `transaction_details_ibfk_1` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`id`),
+  CONSTRAINT `transaction_details_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `transaction_details` */
+
+/*Table structure for table `transactions` */
+
+DROP TABLE IF EXISTS `transactions`;
+
+CREATE TABLE `transactions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `timeout` datetime NOT NULL,
+  `address` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `regency` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `province` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total` double(12,2) NOT NULL,
+  `shipping_cost` double(12,2) NOT NULL,
+  `sub_total` double(12,2) NOT NULL,
+  `user_id` int(20) unsigned NOT NULL,
+  `courier_id` int(10) unsigned NOT NULL,
+  `proof_of_payment` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `status` enum('unverified','verified','delivered','success','expired','canceled') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `courier_id` (`courier_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`courier_id`) REFERENCES `couriers` (`id`),
+  CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `transactions` */
+
+/*Table structure for table `user_notifications` */
+
+DROP TABLE IF EXISTS `user_notifications`;
+
+CREATE TABLE `user_notifications` (
+  `id` char(36) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notifiable_type` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notifiable_id` int(11) unsigned NOT NULL,
+  `data` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `read_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_notifications_notifiable_type_notifiable_id_index` (`notifiable_type`,`notifiable_id`),
+  KEY `notifiable_id` (`notifiable_id`),
+  CONSTRAINT `user_notifications_ibfk_1` FOREIGN KEY (`notifiable_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `user_notifications` */
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `profile_image` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(191) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`name`,`email`,`profile_image`,`status`,`email_verified_at`,`password`,`remember_token`,`created_at`,`updated_at`) values (1,'User','user@gmail.com',NULL,NULL,NULL,'$2y$10$4dK0bDTVezuHESaHI6EtDuZCLSVO1X8PFz3hoY16u1i.sccs3b2rW','Qg26rG53ykV0VB4BRqPfgLz8GKUrohbva5Kr1Eugj4uQa6tOPoKHW9j3M6UD','2021-03-18 20:47:56','2021-03-20 11:29:31'),(2,'User2','user2@gmail.com',NULL,NULL,'2021-03-20 14:13:59','$2y$10$mO746Q4JlO28cOSovW0KouKUvmbADxGV/aE8CO1BuPysmRgmlagcW',NULL,'2021-03-20 14:13:38','2021-03-20 14:13:59');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
